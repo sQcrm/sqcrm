e1a2a4164e18e5425b71bf6b7b4e49a814dea744
Abhik Chakraborty <abhik.chakraborty@gmail.com>
2015-12-24 22:22:53 +0530

    fix for the customview issue

--- modules/PurchaseOrder/list.php
+++ modules/PurchaseOrder/list.php
@@ -4,6 +4,7 @@
 * List view data 
 * @author Abhik Chakraborty
 */  
+$custom_view_allowed = true ;
 if (isset($_GET['ajaxreq']) && $_GET['ajaxreq'] == true) {
 	require_once('view/listview_entry.php');
 } else { 
@@ -11,6 +12,8 @@ if (isset($_GET['ajaxreq']) && $_GET['ajaxreq'] == true) {
 	if (true === $do_custom_view->has_custom_view($module_id)) {
 		$custom_view_data = $do_custom_view->get_custom_views($module_id);
 		$default_custom_view = $do_custom_view->get_default_custom_view($module_id);
+	} else {
+		$custom_view_allowed = false ;
 	}
 	require_once('view/listview.php');
 }
