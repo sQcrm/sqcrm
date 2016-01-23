38108ea9df0ddccc1305c8a99321515963655086
Abhik Chakraborty <abhik.chakraborty@gmail.com>
2016-01-24 00:43:35 +0530

    more library changes

--- includes/bootstrap.footer.inc.php
+++ includes/bootstrap.footer.inc.php
@@ -5,6 +5,7 @@
 * @author Abhik Chakraborty
 */
 ?>
+<script src="/themes/bootstrap/js/bootstrap.js"></script>
 <script src="/themes/bootstrap/js/bootstrap-dropdown.js?v=0.9"></script>
 <script src="/themes/bootstrap/js/bootstrap-modal.js?v=0.9"></script>
 <script src="/themes/bootstrap/js/bootstrap-alert.js?v=0.9"></script>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -6,31 +6,28 @@
 */
 ?>
 <html>
-  <head>
-    <title><?php echo $pageTitle;?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!--<link rel="stylesheet" type="text/css" href="includes/style.css">-->
-    <link href="/themes/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="/themes/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="/themes/custom-css/custom.css" rel="stylesheet">
-    <!--<link href="/themes/custom-css/bootstrap-glyphicons.css" rel="stylesheet">-->
-    <link href="/themes/bootstrap/css/datepicker.css" rel="stylesheet"> 
-    <link href="/themes/bootstrap/css/bootstrap-timepicker.css" rel="stylesheet"> 
+	<head>
+		<title><?php echo $pageTitle;?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="/themes/bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="/themes/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="/themes/custom-css/custom.css" rel="stylesheet">
+		<link href="/themes/bootstrap/css/datepicker.css" rel="stylesheet"> 
+		<link href="/themes/bootstrap/css/bootstrap-timepicker.css" rel="stylesheet"> 
 		<link rel="shortcut icon" href="/themes/images/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="/themes/images/favicon.ico" type="image/x-icon">
-		
-    <meta name="author" content="<?php echo $Author; ?>">
-    <meta name="keywords" content="<?php echo $Keywords; ?>">
-    <meta name="description" content="<?php echo $Description; ?>">
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <?php 
-    $d = dir($cfg_project_directory."includes/");
-    while($entry = $d->read()) {
-        if (preg_match("/\.header\.inc\.php$/i", $entry) && !preg_match("/^\./", $entry)) {
-            include_once($entry);
-        }
-    }
-    $d->close();
-    ?>
+		<meta name="author" content="<?php echo $Author; ?>">
+		<meta name="keywords" content="<?php echo $Keywords; ?>">
+		<meta name="description" content="<?php echo $Description; ?>">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<?php 
+		$d = dir($cfg_project_directory."includes/");
+		while ($entry = $d->read()) {
+			if (preg_match("/\.header\.inc\.php$/i", $entry) && !preg_match("/^\./", $entry)) {
+				include_once($entry);
+			}
+		}
+		$d->close();
+		?>
   </head>
   <body>
