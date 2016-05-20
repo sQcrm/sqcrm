7f3ea2bd917919baeb611d082ca6f7d9ba6865e5
Abhik Chakraborty <abhik.chakraborty@gmail.com>
2016-05-20 21:15:20 +0530

    invoice payment plugin

---
+++ plugins/InvoicePaymentPlugin/asset/i18n_message.js
@@ -0,0 +1,13 @@
+// Copyright SQCRM. For licensing, reuse, modification and distribution see license.txt 
+/*
+* Add all the JS messages related to plugin here
+* The variable names should be as PLUGIN_*
+* @author Abhik Chakraborty
+*/
+
+// Invouce payment related messages
+var PLUGIN_IP_ADD_PAYMENT_DATE = 'Please add payment date' ;
+var PLUGIN_IP_ADD_REF_NUM = 'Please add reference number';
+var PLUGIN_IP_ADD_AMOUNT = 'Please add amount';
+var PLUGIN_IP_AMOUNT_MORE_THAN_DUE = 'Amount added is more than the due amount';
+var PLUGIN_IP_PAYMENT_ADDED = 'Payment added successfully';
---
+++ plugins/InvoicePaymentPlugin/index.php
@@ -0,0 +1,18 @@
+<?php 
+// Copyright SQCRM. For licensing, reuse, modification and distribution see license.txt 
+
+/**
+* @author Abhik Chakraborty
+*/
+$idmodule = (int)$_GET["idmodule"] ;
+$sqcrm_record_id = (int)$_GET["sqrecord"] ;
+
+$module_name = $modules_info[$idmodule]['name'] ;
+$do_module_object = new $module_name();
+
+$do_invoice_payments = new InvoicePaymentPlugin();
+$payments = $do_invoice_payments->get_invoice_payments($sqcrm_record_id);
+$due_amount = FieldType30::display_value($do_invoice_payments->get_due_amount($sqcrm_record_id));
+$payment_modes = $do_invoice_payments->get_payment_modes();
+include_once('view/plugin_view.php');
+?>
